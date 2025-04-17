c06abcb70a1b4320c479eaed4bcd3692c4d6c2c7
Hanif Mianjee <contact@hanifmianjee.me>
2025-04-17 18:30:19 +0500

    updated formulat

--- Formula/git-squash-from.rb
+++ Formula/git-squash-from.rb
@@ -1,8 +1,8 @@
 class GitSquashFrom < Formula
     desc "Interactive CLI to squash commits into a selected base commit"
     homepage "https://github.com/hanif-mianjee/homebrew-git-squash-from"
-    url "https://github.com/hanif-mianjee/homebrew-git-squash-from/archive/refs/tags/v0.1.1.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"  # Run `shasum -a 256 <tarball>` after release
+    url "https://github.com/hanif-mianjee/git-squash-from/archive/refs/tags/v0.1.1.tar.gz"
+    sha256 "c7103f6defa20046cda9126c2d3ec3cd42325223eb9a0f510342b53fa3703ced"  # Run `shasum -a 256 <tarball>` after release
     license "MIT"
   
     def install
